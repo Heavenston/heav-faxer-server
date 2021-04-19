960e16acf8bcd5a4d3cca7c316b0ac9d4d575c74
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-19 04:32:31 +0200

    seed the random generator with time

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"faxer.heav.fr/server/api"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	srv := api.NewServer()
 
 	http.ListenAndServe("0.0.0.0:1234", srv)
