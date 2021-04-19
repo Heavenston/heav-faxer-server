01cacb6cf6547dcb352a029a46152755da3b2dea
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-19 15:07:01 +0200

    use a environement variable for port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"faxer.heav.fr/server/api"
@@ -12,5 +13,10 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	srv := api.NewServer()
 
-	http.ListenAndServe("0.0.0.0:1234", srv)
+	port, isPortSet := os.LookupEnv("PORT")
+	if !isPortSet {
+		port = "1234"
+	}
+
+	http.ListenAndServe("0.0.0.0:"+port, srv)
 }
