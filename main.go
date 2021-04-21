8911ec6983468f2b51b228f9919601caa884c367
Heavenstone <malolegendrelemaire@gmail.com>
2021-04-21 21:43:00 +0200

    Added https

--- main.go
+++ main.go
@@ -18,5 +18,19 @@ func main() {
 		port = "1234"
 	}
 
-	http.ListenAndServe("0.0.0.0:"+port, srv)
+	cert, isCertSet := os.LookupEnv("CERT")
+	key, isKeySet := os.LookupEnv("KEY")
+	if isCertSet && isKeySet {
+		println("Starting the server with port", port, "on https")
+		err := http.ListenAndServeTLS("0.0.0.0:"+port, cert, key, srv)
+		if err != nil {
+			println("Could not start the https server", err.Error())
+		}
+	} else {
+		println("Starting the server with port", port, "on http")
+		err := http.ListenAndServe("0.0.0.0:"+port, srv)
+		if err != nil {
+			println("Could not start the http server", err.Error())
+		}
+	}
 }
